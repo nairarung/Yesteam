207b5f8bce06edf0d62b20d0e23948f16be8a45f
nairarung <70462338+nairarung@users.noreply.github.com>
2022-05-16 13:56:08 +0530

    Delete speech.py

--- speech.py
+++
@@ -1,7 +0,0 @@
-import os
-import Ptxt2spch as ptxt
-
-os.chdir("/home/nairarungster/PText2Speech/CRED/")
-ptxt.text_to_wav("en-AU-Wavenet-A", "What is the temperature in Sydney?")
-
-os.system("cloudshell download en-*.wav")
